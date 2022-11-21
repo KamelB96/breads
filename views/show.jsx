be7969a837a7aeee744e15d5e62b6c06e1a470a6
kamel Barakat <kamelbarakat96@yahoo.com>
2022-11-21 18:43:38 -0500

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -18,14 +18,13 @@ function Show({ bread, index }) {
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name} />
-
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
-                <input type='submit' value="DELETE" />
-            </form>
-
+  <input type='submit' value="DELETE"/>
+  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+</form>
             <li><a href="/breads">Go home</a></li>
-        </Default>
 
+        </Default>
     )
 }
 
